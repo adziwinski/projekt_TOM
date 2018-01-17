e312a5304a26121f443861bd1b638ae5dedf60a6
Andrzej Dziwinski <adziwinski@gmail.com>
2018-01-18 00:25:02 +0100

    dodanie wykresu błędów do multiplicative ART

--- Projekt.m
+++ Projekt.m
@@ -46,10 +46,7 @@ switch choice
             l1=l1+1;
         end
 
-        iteracje = 1:1:(l1-1);
-        F1;
-        blad1;
-        ROZW1;
+        iteracje = 1:(l1-1);
 
         % Rysowanie wykresu
         screenSize = get(groot,'ScreenSize');
@@ -59,9 +56,9 @@ switch choice
         figure('Name','Algorytm Additive ART','pos',windowSize);
 
         subplot(1,2,1)
-         plot(x, y1,'b');
+         plot(x, y(1,:),'b');
          hold on;
-         plot(x, y2, 'r');
+         plot(x, y(2,:), 'r');
          plot(ROZW1(:,1),ROZW1(:,2), 'ko--');
          hold off;
          axis('square')
@@ -111,27 +108,61 @@ switch choice
         blad2 = [0; 0];
         k2 = 1;
         ROZW2(k2,:) = F2;
+        blad2(k2,1) = G(1,1) - (A(1,:) * F2);
+        blad2(k2,2) = G(2,1) - (A(2,:) * F2);
         k2=k2+1;
         for i=1:1:10
             for j=1:1:size(F2,1)
                 f2_1 = F2(j,1) * (G(j,1) / (A(j,:) * F2)) ^ p;
-                blad2(j,1) = G(j,1) - (A(j,:) * F2);
+                blad2(i,j) = G(j,1) - (A(j,:) * F2);
                 F2(j,1) = f2_1;
             end
             ROZW2(k2,:) = F2;
             k2=k2+1;
         end
+        iteracje2 = 1:i;
+       
+        % Rysowanie wykresu
+        screenSize = get(groot,'ScreenSize');
+        windowHigh = screenSize(4);
+        windowSize = screenSize;
+        windowSize(4) = windowHigh * 0.9;
+        figure('Name','Algorytm Multiplicative ART','pos',windowSize);
 
-        F2;
-        blad2;
-        ROZW2;
-
-        % % Rysowanie wykresu
-        figure;
-        plot(x, y1, x, y2, ROZW2(:,1),ROZW2(:,2), 'ko--')
-        axis([1.4 2 1.4 2])
-        grid on
-        grid minor
+        subplot(1,2,1)
+         plot(x, y(1,:),'b');
+         hold on;
+         plot(x, y(2,:), 'r');
+         plot(ROZW2(:,1),ROZW2(:,2), 'ko--');
+         hold off;
+         axis('square')
+         axis([0.9 2 0.9 2])
+         set(0, 'defaultTextFontSize',10);
+         text(0.9,0.65,'Punkt startowy (1, 1)')
+         T = {'Zabaczymy jaki tytul '};
+         title(T)
+         xlabel('x')
+         ylabel('y')
+         grid on
+         grid minor
+         
+        subplot(1,2,2)
+         semilogy(iteracje2, abs(blad2(:,1)), 'x-.');
+         axis('square')
+         title('B³¹d oszacowania i-tej iteracji');
+         xlabel('i')
+         ylabel('b³¹d')
+         grid on
+         grid minor
+         
+         hold on;
+         semilogy(iteracje2, abs(blad2(:,2)), 'rx-.');
+         hold off;
+         legend('równanie nr 1','równanie nr 2');
+         
+         text(-0.2,1.2, 'Algorytm multiplicative ART', 'HorizontalAlignment', 'center',...
+            'FontSize', 16 , 'Units', 'normalized');
+        
 
 %% Pytania:
 % Czy ja te rozwi¹zania w dobrym miejscu wpisujê do puli rozwi¹zañ?
