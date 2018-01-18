daac84b162174e962cf5894747fb96763237a5a5
Andrzej Dziwinski <adziwinski@gmail.com>
2018-01-18 03:58:23 +0100

    przed wykresam 3d

--- Projekt.m
+++ Projekt.m
@@ -2,7 +2,7 @@ clc
 clear all
 close all
 choice1=menu({' Wybierz uk³ad równañ, który chcesz rozwi¹zaæ'}, 'Dwa równania, dwie niewiadome','Trzy równania, dwie niewiadome', 'Dwa równania, trzy niewiadome');
-choice2=menu({' Wybierz metodê rozwi¹zania'}, 'Additive ART','Multiplicative ART ART');
+
 
 % 2 równania 2 niewiadome
 
@@ -14,7 +14,7 @@ switch choice1
         FP = pinv(A)*G; % pseudoodwrotnoœæ
 
         x = 0.9:0.1:2;
-        
+        choice2=menu({' Wybierz metodê rozwi¹zania'}, 'Additive ART','Multiplicative ART ART');
         switch choice2
             case 1 % additive ART
                 additiveART_2rown_2niew( A,G,F,x,10^(-9));
@@ -23,48 +23,51 @@ switch choice1
         end
         
     case 2 %trzy równania, dwie niewiadome
+        clear all;
+        A = [5.05 0.5; 0.1 1; 2 1];
+        G = [8.8421; 1.9721; 4.3452];
+        F = A\G; % równanie jest sprzeczne. FF to rozwi¹zanie œredniokwadratowe
         
+        % rysowanie wykresu
+        x = 1:0.5:2;
+        y = calc2(A,G,x);
+        
+        figure;
+        plot(x, y(1,:), x, y(2,:), x, y(3,:), F(1,1), F(2,1), 'ko')
+        grid on
+        grid minor
     case 3 %dwa równania, trzy niewiadome
+        %AAA = [5.05 0.5 1; 0.1 1 2]; %wychodzi Ÿle
+        clear all;
+        A = [5.05 0.5 2; 0.1 1 1]; %wychodzi dobrze
+        G = [8.5; 2];
+        F = A\G;
+
+        % rysowanie wykresu
+        x = 1:0.01:2;
+        z = zeros(size(x));
+        y = calc3d(A,G,x);
+
+        figure; 
+        plot3(x, y(1,:), z);
+        hold on;
+        plot3(x, y(2,:), z);
+        hold off;
+%         mesh(xxx, yyy1, z??, xxx, yyy2, z??, FFF(1,1), FFF(2,1), FFF(3,1), 'ko')
+        grid on
+        grid minor
 end
     
-%% Pytania:
+% Pytania:
 % Czy ja te rozwi¹zania w dobrym miejscu wpisujê do puli rozwi¹zañ?
 % Wydaje mi siê, ¿e dobrze, bo pierwsza AA ART wykonuje od razu zmianê 
 % x i y, a MART wykonuje najpierw x a w nastêpnym kroku dopiero y
 
 % Jak zrobiæ 2 równania i 3 niewiadome??
 % Jaki jest z??h
-%% 3 równania 2 niewiadome
 
-%         AA = [5.05 0.5; 0.1 1; 2 1];
-%         GG = [8.8421; 1.9721; 4.3452];
-%         FF = AA\GG; % równanie jest sprzeczne. FF to rozwi¹zanie œredniokwadratowe
-%         
-%         % rysowanie wykresu
-%         xx = 1:0.5:2;
-%         yy = calc2(AA,GG,xx);
-%         
-%         figure;
-%         plot(xx, yy(1,:), xx, yy(2,:), xx, yy(3,:), FF(1,1), FF(2,1), 'ko')
-%         grid on
-%         grid minor
-% 
+
 % %% 2 równania 3 niewiadome
 % 
-%         %AAA = [5.05 0.5 1; 0.1 1 2]; %wychodzi Ÿle
-%         AAA = [5.05 0.5 2; 0.1 1 1]; %wychodzi dobrze
-%         GGG = [8.5; 2];
-%         FFF = AAA\GGG;
-% 
-%         % rysowanie wykresu
-%         xxx = 1:0.01:2;
-%         zzz = 1:0.01:2;
-%         yyy1 = ((-(AAA(1,1))*xxx) + (-(AAA(1,3))*xxx) + GGG(1,1)) / AAA(1,2);
-%         yyy2 = ((-(AAA(2,1))*xxx) + (-(AAA(2,3))*xxx) + GGG(2,1)) / AAA(2,2);
-% 
-%         figure; 
-% %         mesh(xxx, yyy1, zzz)
-% %          mesh(xxx, yyy1, z??, xxx, yyy2, z??, FFF(1,1), FFF(2,1), FFF(3,1), 'ko')
-% %         grid on
-% %         grid minor
+
 
--- multiplicativeART_2rown_2niew.m
+++ multiplicativeART_2rown_2niew.m
@@ -1,67 +1,64 @@
 function [ ] = multiplicativeART_2rown_2niew(A,G,F,x, errorSize)
-%UNTITLED3 Summary of this function goes hereinput_args
-%   Detailed explanation goes here
-y = calc2(A,G,x);
 
+    y = calc2(A,G,x);
+    F1 = [1.5; 1.5];
+    p = 0.8;
+    blad = [0; 0];
+    rozwiazanie(1,:) = F1;
 
-        F1 = [1.5; 1.5];
-        p = 0.8;
-        blad = [0; 0];
-        rozwiazanie(1,:) = F1;
-        
-        error = 1;
-        i=1;
-        while error>errorSize
-            for j=1:1:size(F1,1)
-                f2_1 = F1(j,1) * (G(j,1) / (A(j,:) * F1)) ^ p;
-                blad(i,j) = G(j,1) - (A(j,:) * F1);
-                F1(j,1) = f2_1;
-            end
-            error = blad(i,j);    
-            rozwiazanie(i+1,:) = F1;
-            i=i+1;
+    error = 1;
+    i=1;
+    while error>errorSize
+        for j=1:1:size(F1,1)
+            f2_1 = F1(j,1) * (G(j,1) / (A(j,:) * F1)) ^ p;
+            blad(i,j) = G(j,1) - (A(j,:) * F1);
+            F1(j,1) = f2_1;
         end
-        iteracje = 1:i-1;
-       
-        % Rysowanie wykresu
-        screenSize = get(groot,'ScreenSize');
-        windowHigh = screenSize(4);
-        windowSize = screenSize;
-        windowSize(4) = windowHigh * 0.9;
-        figure('Name','Algorytm Multiplicative ART','pos',windowSize);
+        error = blad(i,j);    
+        rozwiazanie(i+1,:) = F1;
+        i=i+1;
+    end
+    iteracje = 1:i-1;
 
-        subplot(1,2,1)
-         plot(x, y(1,:),'b');
-         hold on;
-         plot(x, y(2,:), 'r');
-         plot(rozwiazanie(:,1),rozwiazanie(:,2), 'ko--');
-         hold off;
-         axis('square')
-         axis([0.9 2 0.9 2])
-         set(0, 'defaultTextFontSize',10);
-         text(1.5,1.5,'Punkt startowy (1,5, 1,5)  ','HorizontalAlignment', 'right')
-         T = {'Interpretacja graficzna'};
-         title(T)
-         xlabel('x')
-         ylabel('y')
-         grid on
-         grid minor
-         
-        subplot(1,2,2)
-         semilogy(iteracje, abs(blad(:,1)), 'x-.');
-         axis('square')
-         title('B³¹d oszacowania i-tej iteracji');
-         xlabel('i')
-         ylabel('b³¹d')
-         grid on
-         grid minor
-         
-         hold on;
-         semilogy(iteracje, abs(blad(:,2)), 'rx-.');
-         hold off;
-         legend('równanie nr 1','równanie nr 2');
-         
-         text(-0.2,1.2, 'Algorytm multiplicative ART', 'HorizontalAlignment', 'center',...
-            'FontSize', 16 , 'Units', 'normalized');
+    % Rysowanie wykresu
+    screenSize = get(groot,'ScreenSize');
+    windowHigh = screenSize(4);
+    windowSize = screenSize;
+    windowSize(4) = windowHigh * 0.9;
+    figure('Name','Algorytm Multiplicative ART','pos',windowSize);
+
+    subplot(1,2,1)
+     plot(x, y(1,:),'b');
+     hold on;
+     plot(x, y(2,:), 'r');
+     plot(rozwiazanie(:,1),rozwiazanie(:,2), 'ko--');
+     hold off;
+     axis('square')
+     axis([0.9 2 0.9 2])
+     set(0, 'defaultTextFontSize',10);
+     text(1.5,1.5,'Punkt startowy (1,5, 1,5)  ','HorizontalAlignment', 'right')
+     T = {'Interpretacja graficzna'};
+     title(T)
+     xlabel('x')
+     ylabel('y')
+     grid on
+     grid minor
+
+    subplot(1,2,2)
+     semilogy(iteracje, abs(blad(:,1)), 'x-.');
+     axis('square')
+     title('B³¹d oszacowania i-tej iteracji');
+     xlabel('i')
+     ylabel('b³¹d')
+     grid on
+     grid minor
+
+     hold on;
+     semilogy(iteracje, abs(blad(:,2)), 'rx-.');
+     hold off;
+     legend('równanie nr 1','równanie nr 2');
+
+     text(-0.2,1.2, 'Algorytm multiplicative ART', 'HorizontalAlignment', 'center',...
+        'FontSize', 16 , 'Units', 'normalized');
 end
 
