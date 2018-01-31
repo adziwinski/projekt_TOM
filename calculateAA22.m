39e1b43c3c9f37fa6c01f2afe0fef26851288c40
adziwinski <adziwinski@gmail.com>
2018-01-31 21:46:52 +0100

    edycja additive art; wyrównie długości wektorów

--- calculateAA22.m
+++ calculateAA22.m
@@ -4,22 +4,19 @@ function [rozwiazanie,blad,iteracje] = calculateAA22( A,G,errorSize )
     F1 = [1; 1];
     lambda = 1;
     error = 1;
-    i=1;
     rozwiazanie(1,:) = F1;
     tempBlad = [0; 0];
-    
+    k=2;
     while error>errorSize
-        for j=1:1:size(F1,1)
+        for j=1:1:size(F1)
             f1_1 = F1 + (lambda * ((G(j,:) - (A(j,:)) * F1) / (A(j,:) * A(j,:)')) * A(j,:)');
-            tempBlad(i,j) = G(j) - (A(j,:) * F1);
+            tempBlad(k,j) = G(j) - (A(j,:) * F1);
             F1 = f1_1;
-            rozwiazanie(i+1,:) = F1;
-            error = tempBlad(i,j);          
+            rozwiazanie(k,:) = F1;
+            error = tempBlad(k,j);
+            k=k+1;
         end
-        i=i+1;
     end
-    iteracje = 1:i-1;
+    iteracje = 1:k-1;
     blad = sqrt((tempBlad(:,1)).^2+ (tempBlad(:,2)).^2);
-
-
 end
