b3ab355d5971ff9c0e5e0a8f4d14c8abbc303c29
DESKTOP-HMPIT4D\Weronika <wiera_4444@o2.pl>
2018-02-01 00:18:24 +0100

    działają błędy i rozwiązania

--- calculateAA22.m
+++ calculateAA22.m
@@ -5,22 +5,21 @@ function [rozwiazanie,blad,iteracje] = calculateAA22( A,G,errorSize )
     error = 1;
     rozwiazanie(1,:) = F1;
     tempBlad = (G - (A * F1))';
-    %tempBlad = [0 0'];
     k=2;
     while error>errorSize
         for j=1:1:size(F1)
             f1_1 = F1 + (lambda * ((G(j,:) - (A(j,:)) * F1) / (A(j,:) * A(j,:)')) * A(j,:)');
-            
-            tempBlad(k,j) = G(j) - (A(j,:) * F1);
+            tempBlad(k-1,j) = G(j) - (A(j,:) * F1);
             F1 = f1_1;
             rozwiazanie(k,:) = F1;
-            error = tempBlad(k,j);
+            error = tempBlad(k-1,j);
             k=k+1;
             
         end
     end
-    tempBlad(k,j-1) = G(j-1) - (A(j-1,:) * F1);
+    tempBlad(k-1,j-1) = G(j-1) - (A(j-1,:) * F1);
     iteracje = 1:k-1;
+    iteracje = iteracje - 1;
     blad = sqrt((tempBlad(:,1)).^2+ (tempBlad(:,2)).^2);
 end
     %error = 1;
