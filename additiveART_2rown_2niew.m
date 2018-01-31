1d81520c122a2cede3157cf704caa70ed24187d7
DESKTOP-HMPIT4D\Weronika <wiera_4444@o2.pl>
2018-01-31 21:50:13 +0100

    blad średniokwadratowy nowy plik z lambdą

--- additiveART_2rown_2niew.m
+++ additiveART_2rown_2niew.m
@@ -30,15 +30,56 @@ function [ ] = additiveART_2rown_2niew( A,G,F,x,errorSize)
      text(1,1,'  Punkt startowy (1, 1)','HorizontalAlignment', 'left');
      
     subplot(1,2,2)
-    semilogy(iteracje, abs(blad), 'x-.');
+    semilogy(iteracje, abs(blad(:)), 'x-.');
      axis('square')
-     title('B³¹d oszacowania i-tej iteracji');
-     xlabel('i')
+     title('B³¹d oszacowania dla i-tej iteracji');
+     xlabel('i-ta iteracja')
      ylabel('b³¹d')
      grid minor
 
-
-     text(-0.2,1.2, 'Algorytm additive ART', 'HorizontalAlignment', 'center',...
+     text(-0.2,1.2, 'Algorytm additive ART (const lambda = 1)', 'HorizontalAlignment', 'center',...
         'FontSize', 16 , 'Units', 'normalized');
+    
+    
+    
+    [rozwiazanie,blad,iteracje] = lambdaAA22( A,G, errorSize );
+    % Rysowanie wykresu
+    screenSize = get(groot,'ScreenSize');
+    windowHigh = screenSize(4);
+    windowSize = screenSize;
+    windowSize(4) = windowHigh * 0.9;
+    figure('Name','Algorytm Additive ART','pos',windowSize);
+
+    subplot(1,2,1)
+     plot(x, y(1,:),'b');
+     hold on;
+     plot(x, y(2,:), 'r');
+     plot(rozwiazanie(:,1),rozwiazanie(:,2), 'ko--');
+     hold off;
+     axis('square')
+     axis([0.9 2 0.9 2])
+     set(0, 'defaultTextFontSize',10);
+     text(0.9,0.65,'Punkt startowy (1, 1)')
+     T = {'Ortogonalne rzutowanie punktu na proste'; 
+     'odpowiadaj¹ce dwóm równaniom';
+     'uk³adu z dwiema niewiadomymi'};
+     title(T)
+     xlabel('x')
+     ylabel('y')
+     grid minor
+     text(1,1,'  Punkt startowy (1, 1)','HorizontalAlignment', 'left');
+     
+    subplot(1,2,2)
+    semilogy(iteracje, abs(blad(:)), 'x-.');
+     axis('square')
+     title('B³¹d oszacowania dla i-tej iteracji');
+     xlabel('i-ta iteracja')
+     ylabel('b³¹d')
+     grid minor
+
+     text(-0.2,1.2, 'Algorytm additive ART (lambda zmniejszana w ka¿dej iteracji)', 'HorizontalAlignment', 'center',...
+        'FontSize', 16 , 'Units', 'normalized');
+    
+    
 end
 
--- lambdaAA22.m
+++ lambdaAA22.m
@@ -1,7 +1,7 @@
-function [rozwiazanie,blad,iteracje] = calculateAA22( A,G,errorSize )
+function [rozwiazanie,blad,iteracje] = lambdaAA22( A,G,errorSize )
 
     F1 = [1; 1];
-    lambda = 1;
+    lambda = 1.5;
     %error = 1;
     i=1;
     k=1;
@@ -9,6 +9,7 @@ function [rozwiazanie,blad,iteracje] = calculateAA22( A,G,errorSize )
     tempBlad = G - (A * F1);
     blad(1) = sqrt((tempBlad(1)).^2 + (tempBlad(2)).^2);
     k=k+1;
+    %while error>errorSize
     while blad(i)>errorSize
         for j=1:size(F1,1)
             f1_1 = F1 + (lambda * ((G(j,:) - (A(j,:)) * F1) / (A(j,:) * A(j,:)')) * A(j,:)');
@@ -20,11 +21,11 @@ function [rozwiazanie,blad,iteracje] = calculateAA22( A,G,errorSize )
         end
         i=i+1;
         blad(i) = sqrt((tempBlad(1)).^2 + (tempBlad(2)).^2);
+        
+        lambda = lambda/1.1;
     end
     iteracje = 1:i;
     iteracje = iteracje - 1;
 
 
-
 end
-
