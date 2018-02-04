21df3022cd2c62e52fae1f08fa6c3d93e0a95c71
DESKTOP-HMPIT4D\Weronika <wiera_4444@o2.pl>
2018-02-04 23:25:52 +0100

    Działa MA

--- calcMA.m
+++ calcMA.m
@@ -8,16 +8,16 @@ function [ rozwiazanie,blad,iteracje ] = calcMA( A,G,errorSize, maxAmountOfItera
     rozwiazanie(k,:) = F1;
     k=k+1;
     while blad(k-1)>errorSize && k<maxAmountOfIterations
-        for j=1:1:size(A,1)
-%             f2_1 = F1(j,1) * (G(j,1) / (A(j,:) * F1)) ^ lambda;
-            f2_1 = F1 * (G(j) / (A(j,:) * F1)) ^ lambda;
-            tempBlad(j) = G(j,1) - (A(j,:) * F1);
-            F1 = f2_1;
+        for j=1:size(A,1)
+            for i=1:size(F1,1)
+                f2_1 = F1(i,1) * (G(j) / (A(j,:) * F1)) ^ (lambda*A(j,i));
+                F1(i,1) = f2_1;
+            end
+            tempBlad = G - (A * F1);
+            rozwiazanie(k,:) = F1;
+            blad(k) = sum(abs(tempBlad).^2)^(1/2);
+            k=k+1;
         end   
-        rozwiazanie(k,:) = F1;
-        blad(k) = sum(abs(tempBlad).^2)^(1/2);
-        k=k+1;
-        
         lambda = lambda/lambdaDivideCoeff;
     end
     iteracje = 1:k-1;
